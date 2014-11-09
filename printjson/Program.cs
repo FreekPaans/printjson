d31bff128b4b412b83ab0aefd5ac408beb236a92
freek <freek@infi.nl>
2014-11-09 10:35:40 +0100

    initial commit

---
+++ printjson/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace printjson {
+	class Program {
+		static void Main(string[] args) {
+			var @in = Console.In.ReadToEnd();
+			try {
+				var pretty = JsonConvert.SerializeObject(JToken.Parse(@in),Formatting.Indented);
+
+				Console.WriteLine(pretty);
+			}
+			catch(JsonReaderException) {
+				Console.Error.WriteLine("Couldn't parse json");
+				Console.WriteLine(@in);
+			}
+		}
+	}
+}
